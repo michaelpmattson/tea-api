59f61bee3c248c3b2f4f3d3fd934e8e635021e0e
Michael Mattson <michael.p.mattson@gmail.com>
2022-01-03 18:35:31 -0700

    Add describe block

--- spec/serializers/api/v1/subscription_serializer_spec.rb
+++ spec/serializers/api/v1/subscription_serializer_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'subscription serializer' do
-  let(:subscription) { create(:subscription) }
+  describe 'initialize' do
+    let(:subscription) { create(:subscription) }
 
-  it 'serializes subscription object into json' do
-    json = Api::V1::SubscriptionSerializer.new(subscription).serializable_hash
+    it 'serializes subscription object into json' do
+      json = Api::V1::SubscriptionSerializer.new(subscription).serializable_hash
 
-    expect(json).to be_a(Hash)
-    expect(json).to have_key(:data)
-    expect(json[:data]).to be_a(Hash)
-    expect(json[:data][:id]).to be_a(String)
-    expect(json[:data][:type]).to eq(:subscription)
-    expect(json[:data][:attributes][:customer_id]).to be_an(Integer)
-    expect(json[:data][:attributes][:frequency]).to be_an(Integer)
-    expect(json[:data][:attributes][:status]).to be_a(String)
-    expect(json[:data][:attributes][:title]).to be_a(String)
+      expect(json).to be_a(Hash)
+      expect(json).to have_key(:data)
+      expect(json[:data]).to be_a(Hash)
+      expect(json[:data][:id]).to be_a(String)
+      expect(json[:data][:type]).to eq(:subscription)
+      expect(json[:data][:attributes][:customer_id]).to be_an(Integer)
+      expect(json[:data][:attributes][:frequency]).to be_an(Integer)
+      expect(json[:data][:attributes][:status]).to be_a(String)
+      expect(json[:data][:attributes][:title]).to be_a(String)
+    end
   end
 end
