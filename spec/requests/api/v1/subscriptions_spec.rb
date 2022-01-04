48ecc00616d5dbeef84ff51c50818feb328ec258
Michael Mattson <michael.p.mattson@gmail.com>
2022-01-03 17:08:51 -0700

    Add tests for created subscription, response

--- spec/requests/api/v1/subscriptions_spec.rb
+++ spec/requests/api/v1/subscriptions_spec.rb
@@ -18,21 +18,23 @@ RSpec.describe '/api/v1/subscriptions' do
         expect { post api_v1_subscriptions_path, params: params}.to change {Subscription.count}
       end
 
-      # it 'returns the created subscription' do
-      #   post api_subscriptions_path, params: params
-      #
-      #   expect(response_hash).to include(params)
-      # end
-      #
-      # it 'updates the subscription rating' do
-      #   expect { post api_subscriptions_path, params: params}.to change {Book.first.rating }
-      # end
-      #
-      # it 'returns a 201 response' do
-      #   post api_subscriptions_path, params: params
-      #
-      #   expect(response.status).to eq(201)
-      # end
+      it 'returns the created subscription' do
+        post api_v1_subscriptions_path, params: params
+
+        expect(response_hash).to include(params)
+      end
+
+      it 'returns a default status' do
+        post api_v1_subscriptions_path, params: params
+
+        expect(response_hash).to include(status: "active")
+      end
+
+      it 'returns a 201 response' do
+        post api_v1_subscriptions_path, params: params
+
+        expect(response.status).to eq(201)
+      end
     end
 
     context 'when unsuccessful'
